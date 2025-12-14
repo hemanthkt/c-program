d0a27fa7b6142f6bdd5946abecbac3dc02afc062
hemanthkt <hmkt1503@gmail.com>
2025-12-14 16:10:20 +0530

    minimum occurence

---
+++ atcoder/sol.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char str[100];
+    scanf("%s", str);
+
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (strlen(str) < n)
+        {
+        }
+    }
+}
--- string/cfw/minOcc.c
+++ string/cfw/minOcc.c
@@ -5,20 +5,42 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
+#define MAX_CHARS 255
+#define MAX_SIZE 100
 
-void minOcc(char str[]) int main()
+int main()
 {
     char str[100];
     scanf("%[^\n]", str);
 
-    minOcc(str);
-}
+    int freq[MAX_CHARS];
+    // setting all to 0
+    for (int i = 0; i < MAX_CHARS; i++)
+    {
+        freq[i] = 0;
+    }
 
-void minOcc(char str[])
-{
     int i = 0;
     while (str[i] != '\0')
     {
-        y
+        int asci = (int)str[i];
+        freq[asci]++;
+        i++;
     }
+
+    int min = 0;
+
+    for (i = 0; i < MAX_CHARS; i++)
+    {
+        if (freq[i] != 0)
+        {
+            if (freq[min] > freq[i] || freq[min] == 0)
+            {
+                min = i;
+            }
+        }
+    }
+
+    printf("Minimum occuring charecter is %c, %d times", min, freq[min]);
 }
